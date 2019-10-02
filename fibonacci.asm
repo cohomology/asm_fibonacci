c3e7d993ff68343f9ddf4aa61c751047b1d36b3c
Kilian Kilger <kkilger@gmail.com>
2019-10-03 01:29:41 +0200

    First working version of addition

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,6 +1,7 @@
 ;; Simple fibonacci number generator with arbitrary precision.
 ;; Design goals: - Follow x86_64/amd64 Linux ABI
 ;;               - Try to be efficient, but stay sane  
+;;               - No external libs, no c library calls
 
 bits 64                                                                ; enable 64 bit mode
 global _start                                                          ; expose _start as entry point for the linker
@@ -9,19 +10,19 @@ section .data                                                          ; data se
 zero     db '0'
 align 32
 number1_start:
-db '00000000000000000000000000000009'
+db '00000000000000000000000000000919'
 number1_end: 
 align 32
 number2_start:
-db '00000000000000000000000000000002'
+db '00000000000000000000000000009979'
 number2_end: 
 
 section	.text                                                          ; text (=code) section
 
 _start: mov rdi, number1_end
-        mov rsi, 1
+        mov esi, 3
         mov rdx, number2_end
-        mov rcx, 1
+        mov ecx, 4
         call add
         mov eax, 0x01
         mov edi, 0x01
@@ -34,35 +35,47 @@ _start: mov rdi, number1_end
 
 ;; The add routine adds two decimal numbers a and b, i.e. computes
 ;;                    a = a + b 
-;; The numbers are stored in ASCII form in memory. 
-;; It is assumed that a <= b. It is also assumed that the 
-;; numbers are multiple of 32 bytes long, possible filled with 
-;; '0' on the left. 
+;; The numbers are stored in ASCII form in memory and the addition
+;; happens from right to left, i.e. additional bytes are added to the
+;; front(!) of a, by the schoolbook method.
+;; Preconditions: 
+;;           - We assume a <= b
+;;           - a has enough space on the left to hold digits(b)+1 bytes 
 ;; rdi = pointer to one byte after the end of "a" 
-;; rsi = length of "a" in 32-byte chunks 
-;; rdx = pointer to one byte afte the end of "b"
-;; rcx = length of "b" in 32 byte chunks
-;; Return value: none
-add:     mov eax, 0x30                ; broadcast '0' to all bytes of xmm0
-         movd xmm6, eax               ; mov '0' to zero'th byte of xmm0
-         pxor xmm0, xmm1              ; sets all bytes of xmm1 to 0 => shuffle mask 
-         pshufb xmm6, xmm0            ; broadcast with mask xmm1
-         mov eax, 0x39
-         movd xmm7, eax
-         pshufb xmm7, xmm0
-         mov xmm8, xmm7
-         sub rdi, 0x10             
-         sub rdx, 0x10
-         dec rsi
-         dec rcx
-         movdqa xmm0, oword [rdi]     ; move memory portion of "a" to xmm1
-         movdqa xmm1, oword [rdx]     ; move memory portion of "b" to xmm2
-         psubb xmm0, xmm6
-         psubb xmm1, xmm6
-         paddb xmm0, xmm1 
-         paddb xmm0, xmm6
-         pmaxub xmm7, xmm0
-         psubb xmm7, xmm8
-         movdqa oword [rdi], xmm0
-add_end: ret
+;; rsi = length of "a" in bytes 
+;; rdx = pointer to one bytes afte the end of "b"
+;; rcx = length of "b" in bytes
+;; Return value: rax = new length of a in bytes
+add:      xor r8d, r8d                 ;; r8d = 1 (overflow) = 0 (no overflow)
+          mov r9d, ecx                 ;; save length of b to compute return value 
+add_loop: dec rdi                      ;; decrease both pointers
+          dec rdx 
+          sub ecx, 1                   ;; decrease length of larger number
+          jc add_last                  ;; length = 0 => quit and handle last carry
+          mov r10b, [byte rdx]         ;; r10b = current byte of larger number 
+          sub esi, 1                   ;; decrease length of smaller number
+          jc add_ze 
+          mov al, [byte rdi]
+          sub al, '0'
+add_comp: sub r10b, '0'
+          add al, r10b
+          add al, r8b
+          cmp al, 10
+          jb add_clr
+          sub al, 10
+          mov r8b, 1
+          jmp add_str
+add_clr:  xor r8w, r8w
+add_str:  add al, '0' 
+          mov [byte rdi], al
+          jmp add_loop
+add_last: test r8b, r8b 
+          mov eax, r9d
+          je add_end 
+          inc eax
+          mov cl, '1'
+          mov [byte rdi], cl  
+add_end:  ret
+add_ze:   xor al, al 
+          jmp add_comp 
      
